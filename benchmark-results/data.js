981976310fe29b36c830be18534d7af0cd41a7c4
github-action-benchmark <github@users.noreply.github.com>
2024-04-10 08:17:45 +0000

    add Benchmark (benchmarkdotnet) benchmark result for 32935338c6e67f90df91d6d0882e5af5c10f2611

---
+++ benchmark-results/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1712737065012,
+  "repoUrl": "https://github.com/Blazique/Blazique",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "me@mauricepeters.dev",
+            "name": "Maurice CGP Peters"
+          },
+          "committer": {
+            "email": "me@mauricepeters.dev",
+            "name": "Maurice CGP Peters"
+          },
+          "distinct": true,
+          "id": "32935338c6e67f90df91d6d0882e5af5c10f2611",
+          "message": "Update benchmark workflow to use PUSH_BENCHMARK_RESULTS secret in benchmark.yml",
+          "timestamp": "2024-04-10T10:16:44+02:00",
+          "tree_id": "7724d1d2eafbed8c3cb414adcf7e39a6cccc21e9",
+          "url": "https://github.com/Blazique/Blazique/commit/32935338c6e67f90df91d6d0882e5af5c10f2611"
+        },
+        "date": 1712737064465,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Blazique.Benchmarks.Counter.BuildRenderTreeBenchmarks.BuildRenderTree",
+            "value": 23736.09683159722,
+            "unit": "ns",
+            "range": "± 8298.46209370939"
+          },
+          {
+            "name": "Blazique.Benchmarks.Counter.BuildRenderTreeBenchmarks.BuildRenderTreeWithBlazique",
+            "value": 24499.23624785959,
+            "unit": "ns",
+            "range": "± 1194.5816259236874"
+          }
+        ]
+      }
+    ]
+  }
+}
